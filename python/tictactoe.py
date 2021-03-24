70a028a084bd46d42ffc4447c5fab24c050ecd72
Andrew Parry <andrew@parry.ws>
2021-03-23 22:26:55 -0600

    Added some more buggy features to the tictactoe script.

--- python/tictactoe.py
+++ python/tictactoe.py
@@ -1,49 +1,61 @@
 from guessNumber import fancyPrint, fancyInput
 import random
+import math
 
-board = [
-        [1, 2, 3, 5, 6, 2, 4],
-        [4, 5, 6, 3, 4, 3, 7],
-        [7, 8, 9, 2, 5, 2, 9],
-    ]
+def makeBoard(size):
+    board = []
+    board.append(size)
+    for i in range(size**2):
+        board.append(i+1)
+    return board
 
-def printboard(board):
+def printBoard(board):
     height = len(board)
-    width = len(board[0])
-    for row, y in enumerate(board):
-        
-        for space in range(width):
+    width = len(board)
+    for y in range(board[0]):
+        for space in range(board[0]):
             if space != 0:
                 print('|', end='')
             print('     ', end='')
         print('')
         
-        for column, x in enumerate(y):
-            
-            if column != 0:
+        for char in range(board[0]):
+            if char != 0:
                 print('|', end='')
-            print('  ' + str(x) + '  ', end='')
+            print('  ' + str(board[1+(y*board[0])+char]) + '  ', end='')
         print('')
 
-        for space in range(width):
+        for space in range(board[0]):
             if space != 0:
                 print('|', end='')
-            if row != height-1:
+            if y != board[0]-1:
                 print('_____', end='')
             else:
                 print('     ', end='')
         print('')
-        
 
-        
+testBoard = [3, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def checkBoard(board):
+    #this will check and return a 'o' if o wins, and a 'x' if x wins.
+    for y, rows in enumerate(board):
+        comparison = board[(y*board[0]) + 1]
+        counter = 0
+        for x, character in enumerate(board):
+            if character == [(y*board[0]) + x + 1]:
+                counter += 1
+        if counter == board[0]:
+            return(comparison)
 def tictactoe():
     fancyPrint('Let\'s play some tic tac toe.')
-    board = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-    ]
+    board = makeBoard(3)
+    availableSpaces = []
+    
     print('Let\s flip a coin to see who goes first.')
     myturn = random.randrange(0,2)
+    while True:
+        if myturn == 0:
+            return(7)
 
-printboard(board)
+
+print(checkBoard(testBoard))
