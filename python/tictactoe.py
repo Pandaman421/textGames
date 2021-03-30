c1eff69f688922fea3f413dadfae96aa1cf7f7e3
Andrew Parry <andrew@parry.ws>
2021-03-29 18:26:47 -0600

    finished tictactoe game.

--- python/tictactoe.py
+++ python/tictactoe.py
@@ -33,8 +33,9 @@ def printBoard(board):
             else:
                 print('     ', end='')
         print('')
+    print('')
 
-testBoard = [3, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+testBoard = [3, 'o', 2, 'o', 4, 'o', 6, 'o', 8, 'o']
 
 def checkBoard(board):
     #this will check and return a 'o' if o wins, and a 'x' if x wins.
@@ -57,88 +58,117 @@ def checkBoard(board):
     diagonal = []
     for num in range(board[0]):
         diagonal.append(board[1+num*(1+board[0])])
-    if all(elem == tempArray[0] for elem in tempArray):
-        return(tempArray[0])
+    if all(elem == diagonal[0] for elem in diagonal):
+        return(diagonal[0])
     
     diagonal = []
     for num in range(board[0]):
         diagonal.append(board[0]+(num*(board[0]-1)))
-    if all(elem == tempArray[0] for elem in tempArray):
-        return(tempArray[0])
+    if all(elem == diagonal[0] for elem in diagonal):
+        return(diagonal[0])
     
     #Now check for a cat
-    
+    cat = 1
     for num in board[1:]:
-        if type(num) == int:
-            break
-def tictactoe():
-    fancyPrint('Let\'s play some tic tac toe.')
-    board = makeBoard(3)
-    while True:
-        gameMode = input('How many people are playing? (1/2):')
-        if gameMode == '1' or gameMode == '2':
-            gameMode = int(gameMode)
-            break
-        else:
-            print('Invalid Number of players.')
+        if num != 'o' and num != 'x':
+            cat = 0
+    if cat:
+        return 'cat'
     
-    print('Let\s flip a coin to see who goes first.')
-    print('')
-    turn = random.randrange(0,2)
-    while True:
-        if turn:
-            if gameMode == 1:
-                print('Your turn!')
-            if gameMode == 2:
-                print('Player one\'s turn.')
-            while turn:
-                printBoard(board)
-                try:
-                    spot = int(input('Pick a spot:'))
-                    if spot in(board[1:]):
-                        board[spot] = 'x'
-                        turn = 0
-                    else:
-                        print('Sorry that spot is not available.')
-                except:
-                    print('Sorry that\'s not a valid input.')
-                print('')
-        else:
-            if gameMode == 1:
-                print('Ok my turn.')
-                while not turn:
-                    AIturn = random.randrange(1, len(board))
-                    if board[AIturn] == AIturn:
-                        board[AIturn] = 'o'
-                        turn = 1
-            if gameMode == 2:
-                print('Player two\'s turn.')
-                while not turn:
-                    printBoard(board)
+def tictactoe():
+    playing = 1
+    while playing:
+        fancyPrint('Let\'s play some tic tac toe.')
+        board = makeBoard(3)
+        while True:
+            gameMode = fancyInput('How many people are playing? (1/2):')
+            if gameMode == '1' or gameMode == '2':
+                gameMode = int(gameMode)
+                break
+            else:
+                print('Invalid Number of players.')
+        
+        printBoard(board)
+        fancyPrint('Let\s flip a coin to see who goes first.')
+        fancyPrint('. . . ', 1)
+        print('')
+        turn = random.randrange(0,2)
+        while True:
+            if checkBoard(board) == 'x':
+                if gameMode == 1:
+                    fancyPrint('Gongrats, you win!')
+                    break
+                if gameMode == 2:
+                    fancyPrint('Player 1 wins!')
+                    break
+            if checkBoard(board) == 'o':
+                if gameMode == 1:
+                    fancyPrint('Haha, looks like I win this one.')
+                    break
+                if gameMode == 2:
+                    fancyPrint('Player 2 wins!')
+                    break
+            if checkBoard(board) == 'cat':
+                fancyPrint('Looks like we got a draw.')
+                break
+            
+            if turn:
+                if gameMode == 1:
+                    fancyPrint('Your turn!')
+                if gameMode == 2:
+                    fancyPrint('Player one\'s turn.')
+                while turn:
                     try:
-                        spot = int(input('Pick a spot:'))
+                        spot = int(fancyInput('Pick a spot:'))
                         if spot in(board[1:]):
-                            board[spot] = 'o'
+                            board[spot] = 'x'
                             turn = 0
                         else:
-                            print('Sorry that spot is not available.')
+                            fancyPrint('Sorry that spot is not available.')
+                            printBoard(board)
                     except:
-                        print('Sorry that\'s not a valid input.')
+                        fancyPrint('Sorry that\'s not a valid input.')
+                        printBoard(board)
                     print('')
-        if checkBoard(board) == 'x':
-            if gamemode == 1:
-                print('Gongrats, you win!')
+                printBoard(board)
+            else:
+                if gameMode == 1:
+                    fancyPrint('Ok my turn.')
+                    while not turn:
+                        AIturn = random.randrange(1, len(board))
+                        if board[AIturn] == AIturn:
+                            board[AIturn] = 'o'
+                            turn = 1
+                    printBoard(board)
+                if gameMode == 2:
+                    fancyPrint('Player two\'s turn.')
+                    while not turn:
+                        try:
+                            spot = int(fancyInput('Pick a spot:'))
+                            if spot in(board[1:]):
+                                board[spot] = 'o'
+                                turn = 0
+                            else:
+                                fancyPrint('Sorry that spot is not available.')
+                                printBoard(board)
+                        except:
+                            fancyPrint('Sorry that\'s not a valid input.')
+                            printBoard(board)
+                        print('')
+                    printBoard(board)
+        while True:
+            playAgain = fancyInput('Would you like to play again? (y/n):')
+            playAgaion = playAgain.lower()
+            if playAgain == 'y' or playAgain == 'yes':
                 break
-            if gameMode == 2:
-                print('Player 1 wins!')
+            elif playAgain == 'n' or playAgain == 'no':
+                playing = 0
                 break
-        if checkBoard(board) == 'o':
-            if gameMode == 1:
-                print('Haha, looks like I wil this one.')
-                break
-            if gameMode == 2:
-                print('Player 2 wins!')
-                break
-        if checkBoard(board) == 'cat':
-            print('Looks like we got a draw.')
-            break
+            else:
+                fancyPrint('Sorry that is not a valid input.')
+        print('')
+
+
+
+
+tictactoe()
